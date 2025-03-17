cfa53f5a9f2e47f35b91cf5fa1d105d65e44f1fa
cde113 <cde0113@naver.com>
2025-03-17 23:39:18 +0900

    firsT commit

---
+++ 2nd.py
@@ -0,0 +1,12 @@
+name='최도은'
+print('이름 :' , name)
+
+width = 20
+height = 5
+rectangle_area = width*height
+print('사각형의 면적:' , rectangle_area)
+
+sum=100
+lst=[10, 20, 30]
+total=sum(lst)
+print(total)
---
+++ 3rd.py
@@ -0,0 +1,59 @@
+#2-1
+#print('나의 이름은:', '최도은')
+#print('나의 나이는:', 22)
+#print('나의 키는:',173,'cm입니다.')
+#print('10+20=', 10+20)
+#print('10*20=', 10*20)
+
+#2-4
+#radius=6.0
+#print('원의 반지름', radius)
+#print('원의 면적', 3.14*radius*radius)
+#print('원의 둘레', 2.0*3.24*radius)
+
+#2-6
+#name='홍길동'
+#age=27
+#print('안녕! 나는', name, '이야. 나는 나이가', age, '살이야.')
+#name='홍길순'
+#age=23
+#print('안녕! 나는', name, '이야. 나는 나이가', age, '살이야.')
+
+#2-7
+#my_age=22
+#my_height=177
+#my_age=my_age+1
+#my_height=my_height+1
+#print(my_age, my_height)
+
+
+
+-------------------------------------------------------------------------
+
+
+#my_age=int(input('how old are you?'))
+#print(my_age)
+#print(type(my_age))
+#my_age=my_age+1
+
+#a=True
+#b=False
+#print(a and b)
+#print(a or b)
+#print(not b)
+
+#a=3
+#b=4
+#print(a&b)
+#print(a|b)
+
+#age=int(input('how old are you?'))
+#if age < 20:
+#    print('청소년 할인')
+#print('입장을 환영합니다')
+
+#hour=int(input('What time is it now?'))
+#if hour <12:
+#    print('오전입니다')
+#else:
+#    print('오후입니다')
